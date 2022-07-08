c5fbe94d10e9990f7742e880f676ebd3fd9c259e
Vadim <vizardhd@mail.ru>
2022-07-08 18:18:44 +0800

    a1

---
+++ AnimalList/src/main/java/com/example/animallist/AnimalListApplication.java
@@ -0,0 +1,13 @@
+package com.example.animallist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalListApplication.class, args);
+    }
+
+}
---
+++ AnimalList/src/test/java/com/example/animallist/AnimalListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animallist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
